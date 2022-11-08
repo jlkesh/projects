30103b0b8cb7ad17eaf212d36d88b75225e0713e
jlkesh <john.lgd65@gmail.com>
2022-11-08 16:49:52 +0500

    zcx

--- src/main/java/uz/jl/todoapp/TodoAppApplication.java
+++ src/main/java/uz/jl/todoapp/TodoAppApplication.java
@@ -1,11 +1,15 @@
 package uz.jl.todoapp;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -16,6 +20,8 @@ import javax.annotation.Priority;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @SpringBootApplication
@@ -55,23 +61,34 @@ class Todo {
     private LocalDateTime createdAt;
 
 
+    @Getter
+    @AllArgsConstructor
     public enum Priority {
-        LOW, MEDIUM, HIGH
+        DEFAULT("Default"),
+        LOW("Low"),
+        MEDIUM("Medium"),
+        HIGH("High");
+
+        private final String value;
     }
 
 }
 
 @Controller
 @RequestMapping("/")
+@Slf4j
+@CrossOrigin("*")
 record TodoController(TodoService todoService) {
     @GetMapping
-    public String todoListPage(Model model) {
-        model.addAttribute("todos", todoService.getAll());
+    public String todoListPage(Model model, @RequestParam(required = false, name = "filter") String searchFilter) {
+        log.info(searchFilter);
+        model.addAttribute("todos", todoService.getAll(searchFilter));
         return "todo/todo_list";
     }
 
     @GetMapping("/add")
-    public String addTodoPage() {
+    public String addTodoPage(Model model) {
+        model.addAttribute("priorities", Todo.Priority.values());
         return "todo/todo_add";
     }
 
@@ -98,7 +115,9 @@ record TodoController(TodoService todoService) {
     @GetMapping("/update/{id}")
     public String todoUpdatePage(Model model, @PathVariable Integer id) {
         Todo todo = todoService.get(id);
+        model.addAttribute("priorities", Todo.Priority.values());
         model.addAttribute("todo", todo);
+
         return "todo/todo_update";
     }
 
@@ -107,6 +126,14 @@ record TodoController(TodoService todoService) {
         todo.setId(id);
         todoService.create(todo);
         return "redirect:/";
+    }
+
+    @PostMapping("/update-completed/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void completeTodo( @PathVariable Integer id) {
+        Todo todo = todoService.get(id);
+        todo.setCompleted(!todo.isCompleted());
+        todoService.create(todo);
     }
 }
 
@@ -128,9 +155,18 @@ record TodoService(TodoRepository todoRepository) {
     public void delete(@NonNull Integer id) {
         todoRepository.deleteById(id);
     }
+
+    public List<Todo> getAll(String searchFilter) {
+        if (Objects.nonNull(searchFilter))
+            return todoRepository.findAllByTitleContains(searchFilter);
+        return todoRepository.findAll();
+    }
 }
 
 interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+    @Query("from Todo t where t.title like %:filter%")
+    List<Todo> findAllByTitleContains(@Param("filter") String filter);
 }
 
 // -------------------------------------------------------------------------
